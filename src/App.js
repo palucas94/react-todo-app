d260243a2ed1ad66554dce3584113e174b307dd4
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-24 07:39:31 -0300

    [feat] Form handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form
+        inputText={ inputText }
+        setInputText={ setInputText }
+        todoList={ todoList }
+        setTodoList={ setTodoList }
+      />
       <TodoList />
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 
-function Form() {
+function Form({ inputText, setInputText, todoList, setTodoList }) {
+  const inputHandler = ({ target: { value } }) => {
+    setInputText(value);
+  };
+
+  const randomId = () => Math.random() * 10000;
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTodoList([
+      ...todoList,
+      {
+        id: randomId(),
+        text: inputText,
+        completed: false,
+      }
+    ]);
+    setInputText('');
+  };
+
   return (
     <div className='form-container'>
-      <form className='form-wrapper'>
+      <form className='todo-form'>
         <input
           className='form-input'
           placeholder='Add new to do...'
           type='text'
+          value={ inputText }
+          onChange={ inputHandler }
         />
         <button
           className='form-button'
           type='submit'
+          onClick={ submitHandler }
         >
           Form Button
         </button>
