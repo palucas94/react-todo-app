6c07d5ec039cb19ea9cfa32773c5287297a4e89d
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-24 07:29:41 -0300

    [feat] add TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Form />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function TodoList() {
+  return (
+    <div className="list-container">
+      <div className="item-wrapper">
+        <ul className="todo-list">
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
