b95241b2e92fdfac1d46906c81919c0143e693d6
Lucas Pontes Arruda <paluke94@gmail.com>
2022-03-24 07:12:33 -0300

    [feat] add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className='header-container'>
+      <h1 className='header-title'>TO DO LIST</h1>
+    </header>
+  );
+}
+
+export default Header;
